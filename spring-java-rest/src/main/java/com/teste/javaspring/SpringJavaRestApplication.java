f75b9be912004b0d78bc53515717c4e45ea31cd4
rodrigo <digoaalbuquerque@gmail.com>
2020-10-09 11:05:50 -0300

    Alterada a inicialização de carga da base base

--- spring-java-rest/src/main/java/com/teste/javaspring/SpringJavaRestApplication.java
+++ spring-java-rest/src/main/java/com/teste/javaspring/SpringJavaRestApplication.java
@@ -1,15 +1,10 @@
 package com.teste.javaspring;
 
-import java.io.InputStream;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.teste.javaspring.model.WrapperProduct;
 import com.teste.javaspring.service.ProductService;
 
 @SpringBootApplication
@@ -22,12 +17,7 @@ public class SpringJavaRestApplication {
 	@Bean
 	CommandLineRunner runner(ProductService productService) {
 		return args -> {
-			ObjectMapper objectMapper = new ObjectMapper();
-			TypeReference<WrapperProduct> typeReference = new TypeReference<WrapperProduct>(){};
-			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/data_1.json");
-			WrapperProduct products = objectMapper.readValue(inputStream, typeReference);
-			
-			productService.saveAll(products);
+			productService.initDataBase();
 		};
 	}
 
--- spring-java-rest/src/main/java/com/teste/javaspring/service/ProductService.java
+++ spring-java-rest/src/main/java/com/teste/javaspring/service/ProductService.java
@@ -1,10 +1,16 @@
 package com.teste.javaspring.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.teste.javaspring.model.Product;
 import com.teste.javaspring.model.WrapperProduct;
 import com.teste.javaspring.repository.ProductRepository;
@@ -22,6 +28,40 @@ public class ProductService {
 	public List<Product> findAll(){
 		return productRepository.findAll();
 	}
+	
+	public void initDataBase() {
+
+		List<String> files = Arrays.asList("/json/data_1.json", "/json/data_2.json", "/json/data_3.json",
+				"/json/data_4.json");
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		TypeReference<WrapperProduct> typeReference = new TypeReference<WrapperProduct>() {
+		};
+
+		files.forEach(file -> {
+			InputStream inputStream = null;
+			try {
+				inputStream = TypeReference.class.getResourceAsStream(file);
+				WrapperProduct product = objectMapper.readValue(inputStream, typeReference);
+				List<Product> products = product.getData().parallelStream().distinct().collect(Collectors.toList());
+
+				productRepository.saveAll(products);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					// TODO: handle exception
+				}
+
+			}
+
+		});
+
+	}
+
 	
 	
 }
