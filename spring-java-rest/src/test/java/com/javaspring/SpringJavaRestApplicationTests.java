71c3a237291a4bb3496329349cb6a235f02d1250
rodrigo <digoaalbuquerque@gmail.com>
2020-10-09 15:04:54 -0300

    Retirada anotação @SpringBootTest

--- spring-java-rest/src/test/java/com/javaspring/SpringJavaRestApplicationTests.java
+++ spring-java-rest/src/test/java/com/javaspring/SpringJavaRestApplicationTests.java
@@ -1,9 +1,7 @@
 package com.javaspring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SpringJavaRestApplicationTests {
 
 	@Test
